feebd71034720ea564d05de6acc91c395724e78d
Jaime Campos <jecampos3@wisc.edu>
2025-01-23 16:28:17 -0600

    upload files

---
+++ fifo.sv
@@ -0,0 +1,17 @@
+module FIFO
+#(
+  parameter DEPTH=8,
+  parameter DATA_WIDTH=8
+)
+(
+  input  clk,
+  input  rst_n,
+  input  rden,
+  input  wren,
+  input  [DATA_WIDTH-1:0] i_data,
+  output [DATA_WIDTH-1:0] o_data,
+  output full,
+  output empty
+);
+
+endmodule
---
+++ mac.sv
@@ -0,0 +1,15 @@
+module MAC #
+(
+parameter DATA_WIDTH = 8
+)
+(
+input clk,
+input rst_n,
+input En,
+input Clr,
+input [DATA_WIDTH-1:0] Ain,
+input [DATA_WIDTH-1:0] Bin,
+output [DATA_WIDTH*3-1:0] Cout
+);
+
+endmodule
